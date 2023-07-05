668b6dc12f735aa791e93e75dd1b2ca4162d0d03
Alden  Sullivan <aw.sullivan17@gmail.com>
2023-07-04 22:17:12 -0400

    temp

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,17 @@
 var nodeshout = require('nodeshout');
 const FileReadStream = require('./FileReadStream');
 const ShoutStream = require('./ShoutStream');
-var fs = require('fs');
 import express from 'express';
 
+
 nodeshout.init();
-
-console.log('Libshout version: ' + nodeshout.getVersion());
-
 const app = express();
+
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+app.listen(port);
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   res.send('Hello World!');
 });
 
@@ -38,27 +34,7 @@ app.get('/stream', (req, res) => {
 
 });
 
-app.get('/stream2', (req, res) => {
-  const path = __dirname + '/audio' + '/' + 'juice.mp3';
-
-  var shout = nodeshout.create();
-  shout.setHost('192.53.163.108');
-  shout.setPort(8000);
-  shout.setUser('source');
-  shout.setPassword('VerySecretCode00');
-  shout.setMount('juice');
-  shout.setFormat(1); // 0=ogg, 1=mp3
-  shout.setAudioInfo('bitrate', '192');
-  shout.setAudioInfo('samplerate', '44100');
-  shout.setAudioInfo('channels', '2');
-  shout.open();
-
-  play(shout, path);
-
-});
-
 function play(shout, path) {
-
   const fileStream = new FileReadStream(path, 65536);
   const shoutStream = fileStream.pipe(new ShoutStream(shout));
 
